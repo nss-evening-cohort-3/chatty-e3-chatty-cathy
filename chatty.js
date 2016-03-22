7a2a95a64d9407d041c0510ff813d61e61b222d2
Tim Maddux <timmaddux@Tims-MacBook-Pro.local>
2016-03-21 21:09:25 -0500

    Populating DOM with messages.

--- chatty.js
+++ chatty.js
@@ -6,18 +6,34 @@ let Chatty = (function () {
   let messages = [];
   // the loadCathyquotes is a different request to OPEN and SEND; after send we call that the same way we do functions with ()
   return {
-    loadMessages: function (CB) {
+    loadMessages: function (callBack) {
       let loader = new XMLHttpRequest(); 
       loader.open("GET", "messages.JSON");
       loader.send();
       loader.addEventListener("load", function () {
         // Set the value of the private array
         // this parse command is turning the JSON into js
-      messages = JSON.parse(this.responseText).messages;
-      return console.log(messages) 
+          messages = JSON.parse(this.responseText).messages;
+          callBack(messages);
       });
     },
   }
-
  }());   
+
+function existingMsg(eMsg) {
+    let string = "";
+    let toDom = document.getElementById("messageBox");
+    for (let i = 0; i < eMsg.length; i++) {
+    string += `<div>${eMsg[i].msg}</div>`;
+    console.log(eMsg[i].msg);
+    }
+    toDom.innerHTML = string;
+}
+
+Chatty.loadMessages(existingMsg);
+
+
+
+
+
 
--- newMsg.js
+++ newMsg.js
@@ -1,4 +1,18 @@
+"use strict"
 // Another IIFE should accept an element id and the user message and then add the user's 
 // message - along with the delete button - to the specificed parent element. 
 // Each message should be stored in a private array in this IIFE. 
 // This IIFE should expose a function to read all messages and delete a single message
+
+// function buildNew(msg) {
+//     console.log("newMsg.js", msg);
+// }
+
+
+
+
+
+
+
+
+// Chatty.loadMessages(buildNew);
