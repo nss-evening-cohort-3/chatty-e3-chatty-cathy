95f7939e35abb72d4f5e1c7b9b59a6453a5892a5
Tim Maddux <timmaddux@Tims-MacBook-Pro.local>
2016-03-23 20:28:15 -0500

    Function for private array.

--- newMsg.js
+++ newMsg.js
@@ -3,12 +3,18 @@
 // message - along with the delete button - to the specificed parent element. 
 // Each message should be stored in a private array in this IIFE. 
 // This IIFE should expose a function to read all messages and delete a single message
+Chatty = (function(chatmessages) {
+  let allMessages = [];
 
-// function buildNew(msg) {
-//     console.log("newMsg.js", msg);
-// }
+  chatmessages.getMessages = function () {
+    let message = document.getElementsByClassName('msg');
+    for (let i = 0; i < message.length; i++) {
+      let current = message[i];
+      allMessages.push(current.innerText);
+    };
+    return allMessages;
+  };
 
 
-
-
-// Chatty.loadMessages(buildNew);
+ return chatmessages;
+}(Chatty || {}));
