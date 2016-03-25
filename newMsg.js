d5b6ebb2f812498a2850365ceba97fe3d1d4463e
Tim Maddux <timmaddux@Tims-MacBook-Pro.local>
2016-03-25 10:38:59 -0500

    Add / delete messages added.

--- newMsg.js
+++ newMsg.js
@@ -3,3 +3,28 @@
 // message - along with the delete button - to the specificed parent element. 
 // Each message should be stored in a private array in this IIFE. 
 // This IIFE should expose a function to read all messages and delete a single message
+
+// This function takes our existing messages from the JSON and pushes to the DOM.
+function existingMsg(eMsg) {
+    let string = "";
+    let toDom = document.getElementById("messageBox");
+    for (let i = 0; i < eMsg.length; i++) {
+      string +=  `<div class="msg">${eMsg[i].msg}`;
+      string += "<button type='button' class='btn btn-default btn-xs delete'>";
+      string += "<span class='glyphicon glyphicon-trash' aria-hidden='true'>";
+      string += "</span> delete me!</button></div>";
+    }
+    toDom.innerHTML = string;
+    addMsg(eMsg);
+};
+
+function addMsg (eMsg) {
+    document.getElementById("ack").addEventListener("keyup", function (event) {
+       if (event.keyCode === 13) {
+        eMsg.push("<div class='msg'>"+ event.target.value +"<button type='button' class='btn btn-default btn-xs delete'><span class='glyphicon glyphicon-trash' aria-hidden='true'></span> delete me!</button></div>");
+        document.getElementById("messageBox").innerHTML += eMsg[eMsg.length-1];
+        }
+    });
+};
+
+Chatty.loadMessages(existingMsg);
